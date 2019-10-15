9150270963439b94236001748353be928cf1ea28
sushish-kumar <sushish.kumar.cse14@iitbhu.ac.in>
2019-10-16 02:25:07 +0530

    Update sushish-kumar.java

--- subtraction/sushish-kumar.java
+++ subtraction/sushish-kumar.java
@@ -3,14 +3,14 @@ class solution{
   public static void main(String args[]){
   
     Scanner sn = new Scanner(System.in);
-    int first_number,second_number,summation;
+    int first_number,second_number,difference;
     
     first_number = sn.nextInt();
     second_number = sn.nextInt();
     
-    //summation logic
-    summation = first_number+second_number;
+    //difference logic
+    difference = first_number-second_number;
     
-    System.out.println(summation);
+    System.out.println(difference);
   }
 }
