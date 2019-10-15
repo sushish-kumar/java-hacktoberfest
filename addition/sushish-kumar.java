43cadbdd39509495c48da06d02892db835adbc04
sushish-kumar <sushish.kumar.cse14@iitbhu.ac.in>
2019-10-16 02:24:12 +0530

    Update sushish-kumar.java

--- addition/sushish-kumar.java
+++ addition/sushish-kumar.java
@@ -3,11 +3,14 @@ class solution{
   public static void main(String args[]){
   
     Scanner sn = new Scanner(System.in);
-    int first_number,second_number;
+    int first_number,second_number,summation;
     
     first_number = sn.nextInt();
     second_number = sn.nextInt();
     
-    System.out.println(first_number+second_number);
+    //summation logic
+    summation = first_number+second_number;
+    
+    System.out.println(summation);
   }
 }
