e3799b06967551684e27ad208a272b7a2fadad0f
sushish-kumar <sushish.kumar.cse14@iitbhu.ac.in>
2019-10-16 02:17:02 +0530

    Create sushish-kumar.java

---
+++ addition/sushish-kumar.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class solution{
+  public static void main(String args[]){
+  
+    Scanner sn = new Scanner(System.in);
+    int first_number,second_number;
+    
+    first_number = sn.nextInt();
+    second_number = sn.nextInt();
+    
+    System.out.println(first_number+second_number);
+  }
+}
