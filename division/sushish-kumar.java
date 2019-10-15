ed8df22219df615f7cfb238c8b1a3fad9e961222
sushish-kumar <sushish.kumar.cse14@iitbhu.ac.in>
2019-10-16 02:29:51 +0530

    Create sushish-kumar.java

---
+++ division/sushish-kumar.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class solution{
+  public static void main(String args[]){
+  
+    Scanner sn = new Scanner(System.in);
+    int first_number,second_number,quotient;
+    
+    first_number = sn.nextInt();
+    second_number = sn.nextInt();
+    
+    //quotient logic
+    quotient = first_number+second_number;
+    
+    System.out.println(quotient);
+  }
+}
