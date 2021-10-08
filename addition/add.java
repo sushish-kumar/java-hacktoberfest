fdaadb08a76e198a187be32293036f711ae28c5f
Radha Kumari <56878565+12345radha@users.noreply.github.com>
2021-10-08 15:16:59 +0530

    Create add.java

---
+++ addition/add.java
@@ -0,0 +1,13 @@
+
+
+import java.util.*;
+class solution{
+ public static void main(String[] args)
+  {
+    int first=Integer.parseInt(args[0]);
+    int second=Integer.parseInt(args[1]);
+    
+    int sum=first+second;
+    System.out.println("The sum of two number : " + sum);
+  }
+}
