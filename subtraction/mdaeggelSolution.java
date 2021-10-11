d2752ee689a0a7f74defaf5157059a2ecb19c360
Moritz Däggelmann <moritz.daeggelmann@web.de>
2021-10-11 16:14:37 +0200

    create subtraction solution

---
+++ subtraction/mdaeggelSolution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class solution{
+  public static void main(String args[]){
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Please enter the first number");
+    int n1 = scanner.nextInt();
+    System.out.println("Please enter the second number");
+    int n2 = scanner.nextInt();
+
+    System.out.println(String.format("The result of your calculation is: %s - %s = %s", n1, n2, n1+n2));
+  }
+}
