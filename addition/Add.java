77a87b08ed1e64f0f252a6e04f2944a9728fef23
ROSHNI-KRI <92136886+ROSHNI-KRI@users.noreply.github.com>
2021-10-30 19:06:24 +0530

    Create Add.java
    Added a program to add two integers.

---
+++ addition/Add.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Add
+{
+    public static void main(String arg[])
+    {
+        int x,y;
+        System.out.println("\nEnter two integers");
+        Scanner sn=new Scanner(System.in);
+        x=sn.nextInt();
+        y=sn.nextInt();
+        System.out.println("\nThe Sum of these two numbers is "+(x+y));
+    }
+}
