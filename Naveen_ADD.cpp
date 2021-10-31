dee5ff7047afba03bff26895b8a9efe72a1a6338
Naveen Kumar <69457880+nvn-cgc@users.noreply.github.com>
2021-10-31 11:10:48 +0530

    Create Naveen_ADD.cpp

---
+++ Naveen_ADD.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b,sum=0;
+  cout<<"Enter first number";
+  cin>>a;
+  cout<<"Enter second number";
+  cin>>b;
+  sum=a+b;
+  cout<<"Sum :"<<sum;
+  return0;
+}
