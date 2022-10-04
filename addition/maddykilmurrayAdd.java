dc4f9e218356534f8683be745b637fa5551aeded
Kilmurray <66964622+MaddyKilmurray@users.noreply.github.com>
2022-10-04 11:52:09 +0100

    Addition class added

---
+++ addition/maddykilmurrayAdd.java
@@ -0,0 +1,10 @@
+public class MaddyKilmurrayAdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter a second number");
+        int num2 = scanner.nextInt();
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+    }
+}
